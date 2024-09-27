0f96bce9a255ab5958b366a84c2c2e5465d869a2
ademolaio <samuel.ademola@gmail.com>
2024-09-28 00:56:29 +0200

    pushing what looks like a comprehesive list ready to be use for data analysis.

--- models/exchange_mapping.py
+++ models/exchange_mapping.py
@@ -5,4 +5,7 @@ exchange_dict = {
     'AMS': 'Euronext Amsterdam',
     'NGS': 'NASDAQ', # NGS: NASDAQ Global Select Market. - The highest tier for larger, more established companies.
     'NCM': 'NASDAQ', # NCM: NASDAQ Capital Market - The tier for smaller companies, also known as the NASDAQ SmallCap Market.
+    'ASE': 'NYSE',  # ASE: NYSE - formerly the American Stock Exchange or AMEX
+    'NGM': 'NASDAQ', # NGM = NASDAQ Global Market, a tier of the NASDAQ exchange for mid-sized companies.
+    'BTS': 'CBOE'   # BATS is now part of Cboe and is known as Cboe BZX Exchange.
 }
--- models/robinhood.py
+++ models/robinhood.py
@@ -3,739 +3,733 @@ securities_dict = {
     'AAL':
         {'shares': 1,
          'avg_cost': 11.03,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'ABSI':
         {'shares': 1,
          'avg_cost': 3.89,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'ACCD':
         {'shares': 1,
          'avg_cost': 3.87,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'ADPT':
         {'shares': 1,
          'avg_cost': 5.02,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'AEO':
         {'shares': 1,
          'avg_cost': 21.07,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'AGNC':
         {'shares': 105,
          'avg_cost': 10.43,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'AGRI':
         {'shares': 1,
          'avg_cost': 0.05,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'AHR':
         {'shares': 1,
          'avg_cost': 26.50,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'AIM':
         {'shares': 5,
          'avg_cost': 0.31,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'AMC':
         {'shares': 1,
          'avg_cost': 4.44,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'AMRN':
         {'shares': 4,
          'avg_cost': 0.6075,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'AYTU':
         {'shares': 3,
          'avg_cost': 2.51,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'BB':
         {'shares': 1,
          'avg_cost': 2.17,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BBAI':
         {'shares': 1,
          'avg_cost': 1.58,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BBLG':
         {'shares': 1,
          'avg_cost': 1.71,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'BFLY':
         {'shares': 1,
          'avg_cost': 1.52,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BGS':
         {'shares': 1,
          'avg_cost': 9.25,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BLDE':
         {'shares': 1,
          'avg_cost': 2.93,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BLNK':
         {'shares': 5,
          'avg_cost': 1.84,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BRFS':
         {'shares': 1,
          'avg_cost': 4.60,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'BXMT':
         {'shares': 375,
          'avg_cost': 19.63,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'CAVA':
         {'shares': 131,
          'avg_cost': 63.63,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'CERO':
         {'shares': 5,
          'avg_cost': 0.102,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'CERS':
         {'shares': 1,
          'avg_cost': 1.78,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'CHGG':
         {'shares': 1,
          'avg_cost': 1.79,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'CHPT':
         {'shares': 1,
          'avg_cost': 1.89,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'CLOV':
         {'shares': 1,
          'avg_cost': 2.59,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'CRNC':
         {'shares': 1,
          'avg_cost': 2.68,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'DOLE':
         {'shares': 1,
          'avg_cost': 16.71,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'DRMA':
         {'shares': 1,
          'avg_cost': 1.98,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'DX':
         {'shares': 96,
          'avg_cost': 12.55,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'EARN':
         {'shares': 12,
          'avg_cost': 6.78,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'EAST':
         {'shares': 1,
          'avg_cost': 1.35,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'EC':
         {'shares': 1,
          'avg_cost': 8.80,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'EFC':
         {'shares': 12,
          'avg_cost': 12.09,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'EQNR':
         {'shares': 55,
          'avg_cost': 27.50,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'ET':
         {'shares': 1,
          'avg_cost': 16.05,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'F':
         {'shares': 1,
          'avg_cost': 10.46,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'FFIE':
         {'shares': 1,
          'avg_cost': 0.00,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'FUBO':
         {'shares': 1,
          'avg_cost': 1.39,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'GME':
         {'shares': 1,
          'avg_cost': 22.15,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'GOEV':
         {'shares': 3,
          'avg_cost': 38.49,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'HAIN':
         {'shares': 1,
          'avg_cost': 7.70,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'HEPA':
         {'shares': 2,
          'avg_cost': 0.68,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'HOLO':
         {'shares': 1,
          'avg_cost': 0.24,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'HOTH':
         {'shares': 1,
          'avg_cost': 1.08,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'IBIO':
         {'shares': 5,
          'avg_cost': 1.93,
-         'option_chain': 'n'
+         'options_available': 'n'
          },
 
     'IEP':
         {'shares': 174.25448,
          'avg_cost': 16.96,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'IGC':
         {'shares': 10,
          'avg_cost': 0.369,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'IMRX':
         {'shares': 1,
          'avg_cost': 2.56,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'INTC':
         {'shares': 1,
          'avg_cost': 23.81,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'IOVA':
         {'shares': 1,
          'avg_cost': 11.00,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'ITRM':
         {'shares': 1,
          'avg_cost': 1.15,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'JBLU':
         {'shares': 1,
          'avg_cost': 6.49,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'JEPI':
         {'shares': 60,
          'avg_cost': 57.12,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'JEPQ':
         {'shares': 60,
          'avg_cost': 54.94,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'KPTI':
         {'shares': 10,
          'avg_cost': 0.76,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'LAB':
         {'shares': 1,
          'avg_cost': 1.84,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'LAZR':
         {'shares': 3,
          'avg_cost': 0.91,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'LCID':
         {'shares': 405,
          'avg_cost': 14.26,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'LMND':
         {'shares': 3,
          'avg_cost': 23.42,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'LPCN':
         {'shares': 1,
          'avg_cost': 5.05,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'LQDW':
         {'shares': 74,
          'avg_cost': 28.18,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'LUNR':
         {'shares': 1,
          'avg_cost': 7.55,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'LUXH':
         {'shares': 1,
          'avg_cost': 0.08,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'MLGO':
         {'shares': 5,
          'avg_cost': 0.268,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'MODD':
         {'shares': 1,
          'avg_cost': 2.21,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'MQ':
         {'shares': 2,
          'avg_cost': 5.40,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'MYNA':
         {'shares': 1,
          'avg_cost': 1.48,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'NIO':
         {'shares': 1,
          'avg_cost': 4.91,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'NLY':
         {'shares': 15,
          'avg_cost': 19.60,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'NOK':
         {'shares': 56,
          'avg_cost': 4.45,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'NVO':
         {'shares': 48,
          'avg_cost': 141.54,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'OGEN':
         {'shares': 2,
          'avg_cost': 0.985,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'OPEN':
         {'shares': 7,
          'avg_cost': 2.27,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'OPTT':
         {'shares': 566,
          'avg_cost': 0.2306,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PACB':
         {'shares': 1,
          'avg_cost': 1.57,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PARA':
         {'shares': 1,
          'avg_cost': 10.65,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PATH':
         {'shares': 1,
          'avg_cost': 12.54,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PBR':
         {'shares': 72,
          'avg_cost': 15.20,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PLUG':
         {'shares': 1,
          'avg_cost': 1.70,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PSEC':
         {'shares': 37,
          'avg_cost': 5.22,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PSNL':
         {'shares': 1,
          'avg_cost': 5.07,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PSNY':
         {'shares': 100,
          'avg_cost': 2.26,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'PTON':
         {'shares': 1,
          'avg_cost': 4.87,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'QSI':
         {'shares': 1,
          'avg_cost': 0.98,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'QYLD':
         {'shares': 1,
          'avg_cost': 16.80,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'QYLE':
         {'shares': 1,
          'avg_cost': 25.50,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'RIVN':
         {'shares': 100,
          'avg_cost': 27.98,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'RXRX':
         {'shares': 1,
          'avg_cost': 7.16,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'RYLD':
         {'shares': 1,
          'avg_cost': 15.78,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'SCPX':
         {'shares': 2,
          'avg_cost': 0.86,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'SDIV':
         {'shares': 5,
          'avg_cost': 22.29,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'SEEL':
         {'shares': 0.3125,
          'avg_cost': 3.30,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'SFIX':
         {'shares': 1,
          'avg_cost': 2.25,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'SGMO':
         {'shares': 1,
          'avg_cost': 0.95,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'SNAP':
         {'shares': 1,
          'avg_cost': 10.73,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'SNOA':
         {'shares': 6.5,
          'avg_cost': 5.94,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'SOFI':
         {'shares': 1,
          'avg_cost': 7.75,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'STEM':
         {'shares': 1,
          'avg_cost': 0.32,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'STLA':
         {'shares': 5,
          'avg_cost': 16.75,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'SVOL':
         {'shares': 101.335113,
          'avg_cost': 22.78,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'TLTW':
         {'shares': 83.896645,
          'avg_cost': 25.75,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'TNXP':
         {'shares': 100,
          'avg_cost': 0.5939,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'TOST':
         {'shares': 1,
          'avg_cost': 27.30,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'TPVG':
         {'shares': 31,
          'avg_cost': 8.18,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'TRAW':
         {'shares': 9.8,
          'avg_cost': 9.25,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'TSLA':
         {'shares': 474,
          'avg_cost': 103.75,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'TWO':
         {'shares': 77,
          'avg_cost': 14.19,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'TYLG':
         {'shares': 1,
          'avg_cost': 29.96,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'U':
         {'shares': 1,
          'avg_cost': 19.93,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'UNIT':
         {'shares': 203,
          'avg_cost': 2.69,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'VERB':
         {'shares': 1,
          'avg_cost': 0.07,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'VICI':
         {'shares': 1,
          'avg_cost': 33.18,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'VRAX':
         {'shares': 1,
          'avg_cost': 2.82,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'VUZI':
         {'shares': 1,
          'avg_cost': 1.05,
-         'option_chain': 'Y'
-         },
-
-    'VWAPY':
-        {'shares': 1,
-         'avg_cost': 10.11,
-         'option_chain': 'N'
+         'options_available': 'Y'
          },
 
     'VXRT':
         {'shares': 11,
          'avg_cost': 0.9718,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'VYNE':
         {'shares': 1,
          'avg_cost': 1.90,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'WBA':
         {'shares': 5,
          'avg_cost': 9.13,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'WBX':
         {'shares': 5,
          'avg_cost': 1.35,
-         'option_chain': 'Y'
+         'options_available': 'Y'
          },
 
     'XCUR':
         {'shares': 1,
          'avg_cost': 1.98,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
     'ZOM':
         {'shares': 17,
          'avg_cost': 0.1447,
-         'option_chain': 'N'
+         'options_available': 'N'
          },
 
 }
