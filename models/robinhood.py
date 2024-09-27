2802f374f95262f47525fb407775adbf29fb57f9
ademolaio <samuel.ademola@gmail.com>
2024-09-27 21:19:14 +0200

    commting all my securities currently held in Robinhood.

--- models/robinhood.py
+++ models/robinhood.py
@@ -1,37 +1,741 @@
 securities_dict = {
 
-    'AAPL':
-        {'shares': 50.0,
-         'price_paid': 150.0
-         },  # 50 shares bought at $150 each
-    'MSFT':
-        {'shares': 20.0,
-         'price_paid': 300.0
-         },  # 20 shares bought at $300 each
-    'GOOGL':
-        {'shares': 10.0,
-         'price_paid': 2800.0
-         },  # 10 shares bought at $2800 each
-    'AMZN':
-        {'shares': 5.0,
-         'price_paid': 3500.0
-         },  # 5 shares bought at $3500 each
-    'TSLA':
-        {'shares': 8.0,
-         'price_paid': 700.0
-         }, # 8 shares bought at $700 each
-    'VOO':
-        {'shares': 400.0,
-         'price_paid': 255.00
+    'AAL':
+        {'shares': 1,
+         'avg_cost': 11.03,
+         'option_chain': 'Y'
+         },
+
+    'ABSI':
+        {'shares': 1,
+         'avg_cost': 3.89,
+         'option_chain': 'Y'
+         },
+
+    'ACCD':
+        {'shares': 1,
+         'avg_cost': 3.87,
+         'option_chain': 'Y'
+         },
+
+    'ADPT':
+        {'shares': 1,
+         'avg_cost': 5.02,
+         'option_chain': 'Y'
+         },
+
+    'AEO':
+        {'shares': 1,
+         'avg_cost': 21.07,
+         'option_chain': 'Y'
+         },
+
+    'AGNC':
+        {'shares': 105,
+         'avg_cost': 10.43,
+         'option_chain': 'Y'
+         },
+
+    'AGRI':
+        {'shares': 1,
+         'avg_cost': 0.05,
+         'option_chain': 'N'
+         },
+
+    'AHR':
+        {'shares': 1,
+         'avg_cost': 26.50,
+         'option_chain': 'Y'
+         },
+
+    'AIM':
+        {'shares': 5,
+         'avg_cost': 0.31,
+         'option_chain': 'N'
+         },
+
+    'AMC':
+        {'shares': 1,
+         'avg_cost': 4.44,
+         'option_chain': 'Y'
+         },
+
+    'AMRN':
+        {'shares': 4,
+         'avg_cost': 0.6075,
+         'option_chain': 'Y'
+         },
+
+    'AYTU':
+        {'shares': 3,
+         'avg_cost': 2.51,
+         'option_chain': 'N'
+         },
+
+    'BB':
+        {'shares': 1,
+         'avg_cost': 2.17,
+         'option_chain': 'Y'
+         },
+
+    'BBAI':
+        {'shares': 1,
+         'avg_cost': 1.58,
+         'option_chain': 'Y'
+         },
+
+    'BBLG':
+        {'shares': 1,
+         'avg_cost': 1.71,
+         'option_chain': 'N'
+         },
+
+    'BFLY':
+        {'shares': 1,
+         'avg_cost': 1.52,
+         'option_chain': 'Y'
+         },
+
+    'BGS':
+        {'shares': 1,
+         'avg_cost': 9.25,
+         'option_chain': 'Y'
+         },
+
+    'BLDE':
+        {'shares': 1,
+         'avg_cost': 2.93,
+         'option_chain': 'Y'
+         },
+
+    'BLNK':
+        {'shares': 5,
+         'avg_cost': 1.84,
+         'option_chain': 'Y'
+         },
+
+    'BRFS':
+        {'shares': 1,
+         'avg_cost': 4.60,
+         'option_chain': 'Y'
+         },
+
+    'BXMT':
+        {'shares': 375,
+         'avg_cost': 19.63,
+         'option_chain': 'Y'
+         },
+
+    'CAVA':
+        {'shares': 131,
+         'avg_cost': 63.63,
+         'option_chain': 'Y'
+         },
+
+    'CERO':
+        {'shares': 5,
+         'avg_cost': 0.102,
+         'option_chain': 'N'
+         },
+
+    'CERS':
+        {'shares': 1,
+         'avg_cost': 1.78,
+         'option_chain': 'Y'
+         },
+
+    'CHGG':
+        {'shares': 1,
+         'avg_cost': 1.79,
+         'option_chain': 'Y'
+         },
+
+    'CHPT':
+        {'shares': 1,
+         'avg_cost': 1.89,
+         'option_chain': 'Y'
+         },
+
+    'CLOV':
+        {'shares': 1,
+         'avg_cost': 2.59,
+         'option_chain': 'Y'
+         },
+
+    'CRNC':
+        {'shares': 1,
+         'avg_cost': 2.68,
+         'option_chain': 'Y'
+         },
+
+    'DOLE':
+        {'shares': 1,
+         'avg_cost': 16.71,
+         'option_chain': 'Y'
+         },
+
+    'DRMA':
+        {'shares': 1,
+         'avg_cost': 1.98,
+         'option_chain': 'N'
+         },
+
+    'DX':
+        {'shares': 96,
+         'avg_cost': 12.55,
+         'option_chain': 'Y'
+         },
+
+    'EARN':
+        {'shares': 12,
+         'avg_cost': 6.78,
+         'option_chain': 'Y'
+         },
+
+    'EAST':
+        {'shares': 1,
+         'avg_cost': 1.35,
+         'option_chain': 'N'
+         },
+
+    'EC':
+        {'shares': 1,
+         'avg_cost': 8.80,
+         'option_chain': 'Y'
+         },
+
+    'EFC':
+        {'shares': 12,
+         'avg_cost': 12.09,
+         'option_chain': 'Y'
+         },
+
+    'EQNR':
+        {'shares': 55,
+         'avg_cost': 27.50,
+         'option_chain': 'Y'
+         },
+
+    'ET':
+        {'shares': 1,
+         'avg_cost': 16.05,
+         'option_chain': 'Y'
+         },
+
+    'F':
+        {'shares': 1,
+         'avg_cost': 10.46,
+         'option_chain': 'Y'
+         },
+
+    'FFIE':
+        {'shares': 1,
+         'avg_cost': 0.00,
+         'option_chain': 'Y'
+         },
+
+    'FUBO':
+        {'shares': 1,
+         'avg_cost': 1.39,
+         'option_chain': 'Y'
+         },
+
+    'GME':
+        {'shares': 1,
+         'avg_cost': 22.15,
+         'option_chain': 'Y'
+         },
+
+    'GOEV':
+        {'shares': 3,
+         'avg_cost': 38.49,
+         'option_chain': 'Y'
+         },
+
+    'HAIN':
+        {'shares': 1,
+         'avg_cost': 7.70,
+         'option_chain': 'Y'
+         },
+
+    'HEPA':
+        {'shares': 2,
+         'avg_cost': 0.68,
+         'option_chain': 'N'
+         },
+
+    'HOLO':
+        {'shares': 1,
+         'avg_cost': 0.24,
+         'option_chain': 'N'
+         },
+
+    'HOTH':
+        {'shares': 1,
+         'avg_cost': 1.08,
+         'option_chain': 'N'
+         },
+
+    'IBIO':
+        {'shares': 5,
+         'avg_cost': 1.93,
+         'option_chain': 'n'
+         },
+
+    'IEP':
+        {'shares': 174.25448,
+         'avg_cost': 16.96,
+         'option_chain': 'Y'
+         },
+
+    'IGC':
+        {'shares': 10,
+         'avg_cost': 0.369,
+         'option_chain': 'Y'
+         },
+
+    'IMRX':
+        {'shares': 1,
+         'avg_cost': 2.56,
+         'option_chain': 'N'
+         },
+
+    'INTC':
+        {'shares': 1,
+         'avg_cost': 23.81,
+         'option_chain': 'Y'
+         },
+
+    'IOVA':
+        {'shares': 1,
+         'avg_cost': 11.00,
+         'option_chain': 'Y'
+         },
+
+    'ITRM':
+        {'shares': 1,
+         'avg_cost': 1.15,
+         'option_chain': 'N'
+         },
+
+    'JBLU':
+        {'shares': 1,
+         'avg_cost': 6.49,
+         'option_chain': 'Y'
+         },
+
+    'JEPI':
+        {'shares': 60,
+         'avg_cost': 57.12,
+         'option_chain': 'Y'
+         },
+
+    'JEPQ':
+        {'shares': 60,
+         'avg_cost': 54.94,
+         'option_chain': 'Y'
+         },
+
+    'KPTI':
+        {'shares': 10,
+         'avg_cost': 0.76,
+         'option_chain': 'Y'
+         },
+
+    'LAB':
+        {'shares': 1,
+         'avg_cost': 1.84,
+         'option_chain': 'Y'
+         },
+
+    'LAZR':
+        {'shares': 3,
+         'avg_cost': 0.91,
+         'option_chain': 'Y'
+         },
+
+    'LCID':
+        {'shares': 405,
+         'avg_cost': 14.26,
+         'option_chain': 'Y'
+         },
+
+    'LMND':
+        {'shares': 3,
+         'avg_cost': 23.42,
+         'option_chain': 'Y'
+         },
+
+    'LPCN':
+        {'shares': 1,
+         'avg_cost': 5.05,
+         'option_chain': 'N'
+         },
+
+    'LQDW':
+        {'shares': 74,
+         'avg_cost': 28.18,
+         'option_chain': 'N'
+         },
+
+    'LUNR':
+        {'shares': 1,
+         'avg_cost': 7.55,
+         'option_chain': 'Y'
+         },
+
+    'LUXH':
+        {'shares': 1,
+         'avg_cost': 0.08,
+         'option_chain': 'N'
+         },
+
+    'MLGO':
+        {'shares': 5,
+         'avg_cost': 0.268,
+         'option_chain': 'N'
+         },
+
+    'MODD':
+        {'shares': 1,
+         'avg_cost': 2.21,
+         'option_chain': 'N'
+         },
+
+    'MQ':
+        {'shares': 2,
+         'avg_cost': 5.40,
+         'option_chain': 'Y'
+         },
+
+    'MYNA':
+        {'shares': 1,
+         'avg_cost': 1.48,
+         'option_chain': 'N'
+         },
+
+    'NIO':
+        {'shares': 1,
+         'avg_cost': 4.91,
+         'option_chain': 'Y'
+         },
+
+    'NLY':
+        {'shares': 15,
+         'avg_cost': 19.60,
+         'option_chain': 'Y'
+         },
+
+    'NOK':
+        {'shares': 56,
+         'avg_cost': 4.45,
+         'option_chain': 'Y'
          },
 
     'NVO':
-         {'shares': 131.0,
-          'price_paid': 62.0
-          },
-    'TRAW':{
-        'shares': 9.8,
-        'price_paid': 9.25
-        }
+        {'shares': 48,
+         'avg_cost': 141.54,
+         'option_chain': 'Y'
+         },
+
+    'OGEN':
+        {'shares': 2,
+         'avg_cost': 0.985,
+         'option_chain': 'N'
+         },
+
+    'OPEN':
+        {'shares': 7,
+         'avg_cost': 2.27,
+         'option_chain': 'Y'
+         },
+
+    'OPTT':
+        {'shares': 566,
+         'avg_cost': 0.2306,
+         'option_chain': 'Y'
+         },
+
+    'PACB':
+        {'shares': 1,
+         'avg_cost': 1.57,
+         'option_chain': 'Y'
+         },
+
+    'PARA':
+        {'shares': 1,
+         'avg_cost': 10.65,
+         'option_chain': 'Y'
+         },
+
+    'PATH':
+        {'shares': 1,
+         'avg_cost': 12.54,
+         'option_chain': 'Y'
+         },
+
+    'PBR':
+        {'shares': 72,
+         'avg_cost': 15.20,
+         'option_chain': 'Y'
+         },
+
+    'PLUG':
+        {'shares': 1,
+         'avg_cost': 1.70,
+         'option_chain': 'Y'
+         },
+
+    'PSEC':
+        {'shares': 37,
+         'avg_cost': 5.22,
+         'option_chain': 'Y'
+         },
+
+    'PSNL':
+        {'shares': 1,
+         'avg_cost': 5.07,
+         'option_chain': 'Y'
+         },
+
+    'PSNY':
+        {'shares': 100,
+         'avg_cost': 2.26,
+         'option_chain': 'Y'
+         },
+
+    'PTON':
+        {'shares': 1,
+         'avg_cost': 4.87,
+         'option_chain': 'Y'
+         },
+
+    'QSI':
+        {'shares': 1,
+         'avg_cost': 0.98,
+         'option_chain': 'Y'
+         },
+
+    'QYLD':
+        {'shares': 1,
+         'avg_cost': 16.80,
+         'option_chain': 'Y'
+         },
+
+    'QYLE':
+        {'shares': 1,
+         'avg_cost': 25.50,
+         'option_chain': 'Y'
+         },
+
+    'RIVN':
+        {'shares': 100,
+         'avg_cost': 27.98,
+         'option_chain': 'Y'
+         },
+
+    'RXRX':
+        {'shares': 1,
+         'avg_cost': 7.16,
+         'option_chain': 'Y'
+         },
+
+    'RYLD':
+        {'shares': 1,
+         'avg_cost': 15.78,
+         'option_chain': 'Y'
+         },
+
+    'SCPX':
+        {'shares': 2,
+         'avg_cost': 0.86,
+         'option_chain': 'N'
+         },
+
+    'SDIV':
+        {'shares': 5,
+         'avg_cost': 22.29,
+         'option_chain': 'Y'
+         },
+
+    'SEEL':
+        {'shares': 0.3125,
+         'avg_cost': 3.30,
+         'option_chain': 'N'
+         },
+
+    'SFIX':
+        {'shares': 1,
+         'avg_cost': 2.25,
+         'option_chain': 'Y'
+         },
+
+    'SGMO':
+        {'shares': 1,
+         'avg_cost': 0.95,
+         'option_chain': 'Y'
+         },
+
+    'SNAP':
+        {'shares': 1,
+         'avg_cost': 10.73,
+         'option_chain': 'Y'
+         },
+
+    'SNOA':
+        {'shares': 6.5,
+         'avg_cost': 5.94,
+         'option_chain': 'N'
+         },
+
+    'SOFI':
+        {'shares': 1,
+         'avg_cost': 7.75,
+         'option_chain': 'Y'
+         },
+
+    'STEM':
+        {'shares': 1,
+         'avg_cost': 0.32,
+         'option_chain': 'Y'
+         },
+
+    'STLA':
+        {'shares': 5,
+         'avg_cost': 16.75,
+         'option_chain': 'Y'
+         },
+
+    'SVOL':
+        {'shares': 101.335113,
+         'avg_cost': 22.78,
+         'option_chain': 'Y'
+         },
+
+    'TLTW':
+        {'shares': 83.896645,
+         'avg_cost': 25.75,
+         'option_chain': 'N'
+         },
+
+    'TNXP':
+        {'shares': 100,
+         'avg_cost': 0.5939,
+         'option_chain': 'N'
+         },
+
+    'TOST':
+        {'shares': 1,
+         'avg_cost': 27.30,
+         'option_chain': 'Y'
+         },
+
+    'TPVG':
+        {'shares': 31,
+         'avg_cost': 8.18,
+         'option_chain': 'Y'
+         },
+
+    'TRAW':
+        {'shares': 9.8,
+         'avg_cost': 9.25,
+         'option_chain': 'N'
+         },
+
+    'TSLA':
+        {'shares': 474,
+         'avg_cost': 103.75,
+         'option_chain': 'Y'
+         },
+
+    'TWO':
+        {'shares': 77,
+         'avg_cost': 14.19,
+         'option_chain': 'Y'
+         },
+
+    'TYLG':
+        {'shares': 1,
+         'avg_cost': 29.96,
+         'option_chain': 'Y'
+         },
+
+    'U':
+        {'shares': 1,
+         'avg_cost': 19.93,
+         'option_chain': 'Y'
+         },
+
+    'UNIT':
+        {'shares': 203,
+         'avg_cost': 2.69,
+         'option_chain': 'Y'
+         },
+
+    'VERB':
+        {'shares': 1,
+         'avg_cost': 0.07,
+         'option_chain': 'N'
+         },
+
+    'VICI':
+        {'shares': 1,
+         'avg_cost': 33.18,
+         'option_chain': 'Y'
+         },
+
+    'VRAX':
+        {'shares': 1,
+         'avg_cost': 2.82,
+         'option_chain': 'N'
+         },
+
+    'VUZI':
+        {'shares': 1,
+         'avg_cost': 1.05,
+         'option_chain': 'Y'
+         },
+
+    'VWAPY':
+        {'shares': 1,
+         'avg_cost': 10.11,
+         'option_chain': 'N'
+         },
+
+    'VXRT':
+        {'shares': 11,
+         'avg_cost': 0.9718,
+         'option_chain': 'Y'
+         },
+
+    'VYNE':
+        {'shares': 1,
+         'avg_cost': 1.90,
+         'option_chain': 'N'
+         },
+
+    'WBA':
+        {'shares': 5,
+         'avg_cost': 9.13,
+         'option_chain': 'Y'
+         },
+
+    'WBX':
+        {'shares': 5,
+         'avg_cost': 1.35,
+         'option_chain': 'Y'
+         },
+
+    'XCUR':
+        {'shares': 1,
+         'avg_cost': 1.98,
+         'option_chain': 'N'
+         },
+
+    'ZOM':
+        {'shares': 17,
+         'avg_cost': 0.1447,
+         'option_chain': 'N'
+         },
 
 }
--- utils/fetch_securties.py
+++ utils/fetch_securties.py
@@ -3,18 +3,33 @@ import pandas as pd
 
 from models.column_headers import *
 
+
+
 def fetch_securities(securities_dict):
+    names.clear()
+    sectors.clear()
+    industries.clear()
+    shares.clear()
+    avg_cost.clear()
+
     for securities, data in securities_dict.items():
         stock = yf.Ticker(securities)
         info = stock.info
 
         names.append(info.get('longName', 'N/A'))
+        sectors.append(info.get('sector', 'N/A'))
+        industries.append(info.get('industry', 'N/A'))
+        shares.append(data['shares'])
+        avg_cost.append(data['price_paid'])
 
 
     df = pd.DataFrame({
         'Ticker': securities_dict.keys(),
         'Name': names,
+        'Sectors': sectors,
+        'Industry': industries,
+        'Shares': shares,
+        'Price Paid': avg_cost
     })
 
     return df
-
