2439463df93ffc2e7531a8ccd9c5568970b79a79
ademolaio <samuel.ademola@gmail.com>
2024-09-27 13:37:17 +0200

    submitting working code.

---
+++ models/exchange_mapping.py
@@ -0,0 +1,8 @@
+exchange_dict = {
+    'NMS': 'NASDAQ',        # NMS -> NASDAQ: The middle tier for mid-sized companies.
+    'PCX': 'NYSE',      # PCX -> NYSE Arca
+    'NYQ': 'NYSE',
+    'AMS': 'Euronext Amsterdam',
+    'NGS': 'NASDAQ', # NGS: NASDAQ Global Select Market. - The highest tier for larger, more established companies.
+    'NCM': 'NASDAQ', # NCM: NASDAQ Capital Market - The tier for smaller companies, also known as the NASDAQ SmallCap Market.
+}
--- utils/fetch_securties.py
+++ utils/fetch_securties.py
@@ -1,1 +1,20 @@
 import yfinance as yf
+import pandas as pd
+
+from models.column_headers import *
+
+def fetch_securities(securities_dict):
+    for securities, data in securities_dict.items():
+        stock = yf.Ticker(securities)
+        info = stock.info
+
+        names.append(info.get('longName', 'N/A'))
+
+
+    df = pd.DataFrame({
+        'Ticker': securities_dict.keys(),
+        'Name': names,
+    })
+
+    return df
+
